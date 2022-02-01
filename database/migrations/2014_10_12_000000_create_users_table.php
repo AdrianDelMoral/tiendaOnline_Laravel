e487d8c7c3ce14d420e60d26817c371be189a176
Borja Ferrer <borjetaa27@gmail.com>
2022-02-01 16:40:40 +0100

    naann

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,11 +17,13 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nif');
             $table->string('password');
+            $table->int("address");
+            $table->string('avatar', 2048)->nullable();
+            $table->string("rol");
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
     }
