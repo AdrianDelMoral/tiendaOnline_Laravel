78be5849e84985d22ecf0f7692247fa54adc5bf7
Borja Ferrer <borjetaa27@gmail.com>
2022-02-02 10:41:34 +0100

    Añadidos los avatares

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('nif');
             $table->string('password');
             $table->double("address")->default(0);
-            $table->string('avatar', 2048)->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->string("rol")->default('user');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
