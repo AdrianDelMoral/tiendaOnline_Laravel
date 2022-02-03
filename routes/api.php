c00ae24dd25006e88a59bcb203f36e3cd700f2a7
Borja Ferrer <borjetaa27@gmail.com>
2022-02-03 20:25:53 +0100

    Arreglado el error de las vistas en la API

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('/direccion', [AddressController::class])->only("store");
+Route::apiResource('/direccion', AddressController::class)->only("store");
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
