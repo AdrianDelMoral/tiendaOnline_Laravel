fe5fd576d7b44001a879801e438b186df2e1e401
Borja Ferrer <borjetaa27@gmail.com>
2022-02-07 16:47:47 +0100

    añadidas las imagenes sobre las fichas de los productos

--- resources/views/catalogo/show.blade.php
+++ resources/views/catalogo/show.blade.php
@@ -1,3 +1,9 @@
+
+
 {{$product->nombre}}<br>
 {{$product->descripcion}}<br>
 {{$product->precio_base}}<br>
+@foreach ($product->images as $imagen)
+    <img src="{{asset("images/products-imgs/".$imagen->img_path)}}">
+@endforeach
+
