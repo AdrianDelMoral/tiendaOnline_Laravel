e13d382691681b22896099b876e6b8721638d75d
Adrian Del Moral <iFullStart@gmail.com>
2022-02-08 16:56:54 +0100

    View Catalogo terminada y organizada

---
+++ resources/views/catalogo.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('partials.head'){{-- Se incluirá el trozo de head de partials/nav.blade.php --}}
+    <link rel="stylesheet" href="{{URL::asset('css/catalogo.css')}}">{{-- Se incluirá el css de public/css/catalogo.css --}}
+    <title>@yield('titulo')</title>{{-- Titulo de catalogo --}}
+</head>
+<body>
+    @include('partials.nav'){{-- Se incluirá el nav de partials/nav.blade.php --}}
+    @yield('cuerpo')
+    @include('partials.footer'){{-- Se incluirá el nav de partials/nav.blade.php --}}
+</body>
+</html>
--- resources/views/catalogo/inicio.blade.php
+++ resources/views/catalogo/inicio.blade.php
@@ -1,43 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('catalogo')
+@section('titulo', 'Catalogo de Productos')
 
-<head>
-    @include('partials.head')
-    <link rel="stylesheet" href="{{ URL::asset('css/catalogo.css') }}">
-    <title>Catalogo de Productos</title>
-</head>
+@include('partials.head')
 
-<body>
-    <header>
-        @include('partials.nav')
-    </header>
+@section('cuerpo')
+{{--
+    @foreach ($products as $product)
+        <a href="{{route('catalogo.show', $product)}}">{{$product->nombre}} </a>
+        Precio: {{$product->precio_base}}€<br>
+        Precio Total: {{$product->precio_base - $product->descuento + $product->impuestos}}€
+        Categoria: {{$product->category->nombre}}<br>
+        Descripcion: {{$product->descripcion}}<br>
+        @forelse ($product->images as $imagen)
+            <img src="{{asset("images/products-imgs/".$imagen->img_path)}}">
+        @empty
+        <p>No hay imagenes</p>
+        @endforelse
+    @endforeach
+--}}
     <section class="mt-5 py-5 text-center container">
         <div class="heroBackGround row py-lg-5">
             <div class="col-lg-6 col-md-8 mx-auto">
                 <h1 class="fw-dark">Titulo</h1>
-                <p class="lead text-dark">Something short and leading about the collection below—its contents, the
-                    creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it
-                    entirely.</p>
-                <p>
+                <p class="lead text-dark">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
                     <a href="#" class="btn btn-dark my-2 text-light">Main call to action</a>
                 </p>
             </div>
         </div>
     </section>
-    {{-- @foreach ($products as $product)
-                <a href="{{route('catalogo.show', $product)}}">{{$product->nombre}} </a>
-                Precio: {{$product->precio_base}}€<br>
-                Precio Total: {{$product->precio_base - $product->descuento + $product->impuestos}}€
-                Categoria: {{$product->category->nombre}}<br>
-                Descripcion: {{$product->descripcion}}<br>
-                @forelse ($product->images as $imagen)
-                    <img src="{{asset("images/products-imgs/".$imagen->img_path)}}">
-                @empty
-                <p>No hay imagenes</p>
-                @endforelse
-            @endforeach --}}
 
-    <section class="section-products">
+
+    <main class="section-products">
         <div class="container">
             <div class="row justify-content-center text-center">
                 <div class="col-md-8 col-lg-6">
@@ -66,20 +59,18 @@
                             <div class="part-2">
                                 <h1 class="product-title"><a
                                         href="{{ route('catalogo.show', $product) }}">{{ $product->nombre }} </a></h1>
-                                <h2 class="product-old-price">{{$product->precio_base}}€</h2>
-                                <h2>{{$product->precio_base - $product->descuento + $product->impuestos}}€</h2>
+                                <h2 class="product-old-price">{{ $product->precio_base }}€</h2>
+                                <h2>{{ $product->precio_base - $product->descuento + $product->impuestos }}€</h2>
                                 <br>
-                                <h2 class="product-info">{{$product->category->nombre}}</h2>
+                                <h2 class="product-info">{{ $product->category->nombre }}</h2>
                             </div>
                         </div>
                     </div>
                 @endforeach
             </div>
         </div>
-    </section>
+    </main>
 
-    <footer>
-        @include('partials.footer')
-    </footer>
+@endsection
 </body>
 <html>
