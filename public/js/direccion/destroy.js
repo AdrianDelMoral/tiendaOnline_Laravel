f23c60874bd3aec9c948a90affbc2335e0e2ef2c
angel <angetos231@gmail.com>
2022-02-17 14:21:28 +0100

    Arregladas direcciones 100% funcionan

--- public/js/direccion/destroy.js
+++ public/js/direccion/destroy.js
@@ -1,21 +1,27 @@
-let eliminarHandler = document.querySelector(".table-responsive");
-eliminarHandler.onclick = destroyAddress;
+try{
+    let eliminarHandler = document.querySelector(".table-responsive");
+    eliminarHandler.onclick = destroyAddress;
 
 
-async function destroyAddress(event){
-    if(event.target.id !== "eliminar"){
-        return;
+    async function destroyAddress(event){
+        if(event.target.id !== "eliminar"){
+            return;
+        }
+        console.log(event.target);
+        event.preventDefault();
+        let form = event.target.parentElement;
+        id = form.querySelector("#addressid").value;
+
+        let response = await fetch("/api/direccion/"+id,{
+            method: 'POST',
+            body: new FormData(form)
+        });
+
+        let result = await response.text();
+        console.log(result);
     }
-    console.log(event.target);
-    event.preventDefault();
-    let form = event.target.parentElement;
-    id = form.querySelector("#addressid").value;
 
-    let response = await fetch("/api/direccion/"+id,{
-        method: 'POST',
-        body: new FormData(form)
-    });
-
-    let result = await response.text();
-    console.log(result);
+}
+catch(e){
+    //
 }
